1d9ffa68558cb30f7d5b571308a6088d3b0f85b8
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-09-26 21:19:01 +0500

    wcat implemented 6th & 7th test current failing.
    All test are currently passing but last test 6th & 7th test is failing.

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    for (int i = 0; i < argc; i++)
+    if (argc > 1) // files is specified
     {
-        FILE *to_open = fopen(argv[i + 1], "r");
-        char ch = getc(to_open);
-        while (ch != EOF)
+        for (int i = 1; i < argc; i++)
         {
-            printf("%c", ch);
-            ch = getc(to_open);
-        }
+            FILE *to_open = fopen(argv[i], "r");
+            if (to_open == NULL)
+            {
+                printf("%s\n", "wcat: cannot open file");
+            }
+            else
+            {
+                char ch = getc(to_open);
+                while (ch != EOF)
+                {
+                    printf("%c", ch);
+                    ch = getc(to_open);
+                }
 
-        fclose(to_open);
+                fclose(to_open);
+            }
+        }
+    }
+    else // file not specified
+    {
+        exit(0);
     }
 }
