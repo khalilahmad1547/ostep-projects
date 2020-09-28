53c5d554ef7b7f206e21132693048c44a47fad82
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-09-28 11:33:50 +0500

    Started working on wgrep.

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
     {
         if (argc > 2) // file is specified
         {
-
             for (int i = 2; i < argc; i++)
             {
                 FILE *to_open = fopen(argv[i], "r");
@@ -18,36 +17,12 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
-                    int t_lines = 0;
-                    int c_in_line = 0;
-                    char ch = getc(to_open);
-                    while (ch != EOF)
-                    {
-                        if (ch == '\n')
-                        {
-                            t_lines++;
-                        }
-                        else
-                        {
-                            c_in_line++;
-                        }
-                        ch = getc(to_open);
-                    }
-
-                    printf("\n Total Char:%d Total lines:%d\n", c_in_line, t_lines);
-
-                    fclose(to_open);
                 }
+                fclose(to_open);
             }
         }
-        else
-        {
-        }
     }
-    else // string for seaarch is not specified
+    else
     {
-        printf("%s\n", "wgrep: searchterm [file ...]");
-        exit(0);
     }
-    exit(0);
 }
