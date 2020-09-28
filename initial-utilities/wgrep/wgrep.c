b39f06c4cde18b3585079259d56e2835a5987053
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-09-28 14:54:51 +0500

    Able to Search given strinng in a file.
    A Search(char* str1, char* str2) function is implemented. Which can search str1 in str2.

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int Search(char *str1, char *str2);
 int main(int argc, char *argv[])
 {
     if (argc >= 1) // string is specified
@@ -20,7 +22,17 @@ int main(int argc, char *argv[])
                 {
                     while (fgets(str, 100, to_open) != NULL)
                     {
-                        printf("%s", str);
+                        if (strncmp(argv[1], str, strlen(argv[1])) == 0)
+                        {
+                            printf("%s", str);
+                        }
+                        else
+                        {
+                            if (Search(argv[1], str) == 1)
+                            {
+                                printf("%s", str);
+                            }
+                        }
                     }
                 }
                 fclose(to_open);
@@ -28,15 +40,15 @@ int main(int argc, char *argv[])
         }
         else
         {
-            /*  
+            /*
                 No file is specified
                 standerd input method
             */
         }
-        }
+    }
     else
     {
-        /* 
+        /*
             zero command line arguments
 
          */
@@ -44,4 +56,34 @@ int main(int argc, char *argv[])
         exit(1);
     }
     exit(0);
+}
+
+//////////////////////////
+int Search(char *to_search, char *from)
+{
+    for (int i = 0; i < strlen(from); i++)
+    {
+        if (to_search[0] == from[i])
+        {
+            int matched = 1;
+            int temp = i + 1;
+            for (int j = 1; j < strlen(to_search); j++)
+            {
+                if (from[temp] == to_search[j])
+                {
+                    matched++;
+                    temp++;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (matched == strlen(to_search))
+                {
+                    return 1;
+                }
+            }
+        }
+    }
 }
